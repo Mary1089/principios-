17065d09f84fb85a4df411c1c05a5f4efe79b96c
Mary1089 <mariana.cardonarestrepo@iemanueljbetancur.edu.co>
2025-04-01 21:00:13 -0500

    Add files via upload

---
+++ inicio.php
@@ -0,0 +1,26 @@
+<?php
+include("menu.php");?>
+
+
+<div id="carouselExampleFade" class="carousel slide carousel-fade">
+      <div class="carousel-inner w-70" style="margin-left: 13%;">
+        <div class="carousel-item active">
+          <img src="imagen1.png" class="d-block w-70" style="height: 557px; width: 984px;">
+        </div>
+        <div class="carousel-item">
+          <img src="imagen2.png" class="d-block w-70" style="height: 557px; width: 984px;">
+        </div>
+        <div class="carousel-item">
+          <img src="imagen3.png" class="d-block w-70" style="height: 557px; width: 984px;">
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+    
